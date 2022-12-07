b3c2b3a10a1513ed14f3f93ad19a67370724ebd3
QCam0111 <113458235+QCam0111@users.noreply.github.com>
2022-12-07 17:15:27 -0500

    Update Interpolation.py

--- Interpolation.py
+++ Interpolation.py
@@ -183,5 +183,15 @@ plt.scatter(skull_wavelengths, WM_est_skull_abs, color='darkorchid')
 
 plt.plot(scalp_interp_wv, WM_avg_est_lambda, '--', label='WM Avg b/t Scalp and Skull', color='purple')
 
-plt.legend(bbox_to_anchor=(1.0, 1.0), loc='upper left')
+plt.axvline(x = 1550, color = 'crimson', label = '1550 nm')
+plt.axvline(x = 1300, color = 'black', label = 'End of GM/WM Known Data', linestyle = '--')
+
+# Add 1550 nm absorption labels
+plt.annotate("Scalp Interpolation: " + "{:.3f}".format(scalp_interpolation(1550)) + " cm^-1", xy=(1550, scalp_interpolation(1550)), xytext=(1590, -0.5), arrowprops=dict(arrowstyle="->"))
+plt.annotate("Skull Interpolation: " + "{:.3f}".format(skull_interpolation(1550)) + " cm^-1", xy=(1550, skull_interpolation(1550)), xytext=(1650, 0), arrowprops=dict(arrowstyle="->"))
+plt.annotate("Avg. White Matter Extrapolation: " + "{:.3f}".format(float(WM_avg_est_lambda[np.where(scalp_interp_wv==1550)])) + " cm^-1", xy=(1550, WM_avg_est_lambda[np.where(scalp_interp_wv==1550)]), xytext=(1710, 0.5), arrowprops=dict(arrowstyle="->"))
+plt.annotate("Gray Matter Skull Extrapolation: " + "{:.3f}".format(float(GM_skull_estimation[np.where(skull_interp_wv==1550)])) + " cm^-1", xy=(1550, GM_skull_estimation[np.where(skull_interp_wv==1550)]), xytext=(1780, 1.0), arrowprops=dict(arrowstyle="->"))
+plt.annotate("White Matter Skull Extrapolation: " + "{:.3f}".format(float(WM_skull_estimation[np.where(skull_interp_wv==1550)])) + " cm^-1", xy=(1550, WM_skull_estimation[np.where(skull_interp_wv==1550)]), xytext=(1830, 1.5), arrowprops=dict(arrowstyle="->"))
+
+plt.legend(loc='upper right')
 plt.show()
